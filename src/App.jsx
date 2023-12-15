36531c6d6418f7cd52df1a9ad56c279fd0dc6e17
casti-lob <casti_lob@hotmail.com>
2023-12-15 17:55:00 +0100

    minios terminido

--- src/App.jsx
+++ src/App.jsx
@@ -296,17 +296,97 @@ export default function DrinkList() {
 }
 */
 //Hacer los minios aqui
-const people = [
-  'Creola Katherine Johnson: matemática',
-  'Mario José Molina-Pasquel Henríquez: químico',
-  'Mohammad Abdus Salam: físico',
-  'Percy Lavon Julian: químico',
-  'Subrahmanyan Chandrasekhar: astrofísico'
-];
+const minions =[
+  {
+    name: "Kevin",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "src/assets/img/kevin.jpg",
+    birth: "1951",
+    side:"de los buenos"
+  },
+  {
+    name: "Josua",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "src/assets/img/Josua.jpg",
+    birth: "1672",
+    side:"malvado"
+  },
+  {
+    name: "Dave",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "src/assets/img/dave.jpg",
+    birth: "1723",
+    side: "de los buenos"
+  },
+  {
+    name: "Mudito",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "src/assets/img/mudito.jpeg",
+    birth: "1379",
+    side:"de los buenos"
+  },
+  {
+    name: "Llongueras",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "src/assets/img/llongueras.jpg",
+    birth: "1687",
+    side: "malvado"
+  },
+  {
+    name: "Minioncé",
+    bio: "Le va el cante, dar la nota, ama los karaokes, es el rey y reina de la fiesta. Invítalo a tu fiesta o te arrepentirás.",
+    img: "src/assets/img/minionce.jpg",
+    birth: "1976",
+    side: "de los buenos"
+  },
+  {
+    name: "Lobeznion",
+    bio: "No lo enfades, este bichillo tiene muy malas pulgas...aunque sólo mide medio metro y ¡no puede ser más gracioso!",
+    img: "src/assets/img/lobeznion.jpg",
+    birth: "2017",
+    side: "malvado"
+  },
+  {
+    name: "Minion Presley",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "src/assets/img/minion-presley.jpg",
+    birth: "2017",
+    side: "malvado"
+  }
+
+] 
 
 export default function List() {
-  const listItems = people.map(person =>
-    <li>{person}</li>
+  
+
+  function GoodOrBad({side}){
+    if(side=='malvado'){
+     return <img src='src/assets/img/de los malos.PNG' id='sideImg'  className="card-img-top"  alt="..."></img>
+    }
+    return <img src='src/assets/img/de los buenos.PNG' id='sideImg'  className="card-img-top"   alt="..."></img>
+  }
+  const listItems = minions.map(minion =>
+    
+    <div className="col-lg-4">
+      <div  className="card">
+        <img src={minion.img}  className="card-img-top" alt="..."></img>
+        <div className="card-body">
+          <h5 className="card-title">{minion.name}</h5>
+          <p className="card-text">{minion.bio}</p>
+        </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Año {minion.birth}</li>
+        <li  className="list-group-item">Actitud {minion.side}
+            <GoodOrBad
+              side ={minion.side}
+            />
+           
+        </li>
+
+      </ul>
+      </div>
+    </div>
   );
-  return <ul>{listItems}</ul>;
+  
+  return <div className='container'>{listItems}</div>;
 }
